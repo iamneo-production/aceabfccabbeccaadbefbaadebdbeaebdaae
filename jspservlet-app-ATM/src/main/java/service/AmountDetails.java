65072d9b3e1cf95d21705c9e5d86cb527f838a7d
Iamneo <techadmin@iamneo.ai>
2023-08-01 04:09:51 +0000

    Backup Commit

---
+++ jspservlet-app-ATM/src/main/java/service/AmountDetails.java
@@ -0,0 +1,21 @@
+package service;
+
+public class AmountDetails implements InterfaceClass {
+
+    public double withdrawAmount(double amount, double balance) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public double depositAmount(double amount, double balance) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public double accountBalance(double balance) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+	
+}
